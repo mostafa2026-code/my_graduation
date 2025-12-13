d4d9b918b4c1d544498481340841705652ee6211
mostafa2026-code <mustafaabdelrahim2024@gmail.com>
2025-12-13 10:26:20 +0200

    create chest inspection class

---
+++ lib/core/enums/chest_enums.dart
@@ -0,0 +1,31 @@
+enum SymetricalAbnormality { flat, barrel, pectusExcavatum, pectusCarinatum }
+
+enum ChestRegions { subclavicular, mammary, supramammary, infraMammary }
+
+enum ASymetricalAbnormality { retration, bulging }
+
+enum VisiblePulsation {
+  epigastricPulsation,
+  apicalPulsation,
+  ltParasternalPulsation,
+  left2ndSpacePulsation,
+  right2ndSpacePulsation,
+  suprasternalPulsation,
+}
+
+enum BreathingType { abdominal, thoracic }
+
+enum VertebralColumn{
+  normal,
+  scoliosis,
+  kyphosis,
+  kyphoscoliosis
+}
+
+enum SkinChanges{
+  noChanges,
+  scar,
+  pigmentation,
+  nodules,
+  dilatedVeins,
+}
